027d4fa70dcef176e3866459554d44c6eb267426
kttian <19katherinet@students.harker.org>
2018-08-23 11:45:48 -0700

    pipeline and prepare_data for pyfasta loader, training still not as good

---
+++ scripts/prepare_data_pf.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+
+import sys
+import logging
+import os
+
+logging.basicConfig(
+        format='%(asctime)s %(levelname)-5s %(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+
+logging.info(" ".join(sys.argv))
+
+
+
+"""
+TFNET_ROOT is set to the root of TFNET dir
+ +-- scripts       such as label_region, prepare_data.py, run_deeplift.py, run_modisco.py, run_pipeline.py etc
+ +-- genome        hg19.fa hg19.chrom.sizes
+ +-- ENCODE_data   intervals
+ +-- results       results of the pipeline
+      +-- ZNF143
+      +-- CTCF
+      +-- SIX5
+"""
+ROOT_DIR   = os.getenv('TFNET_ROOT', "../../") 
+scriptDir  = ROOT_DIR + "/scripts/"
+dataDir    = ROOT_DIR + "/ENCODE_data/"
+genomeDir  = ROOT_DIR + "/genome/"
+resultsDir = "./"
+logDir     = resultsDir + "log/"
+#tmpDir     = "./tmp/"
+
+if len(sys.argv) > 4:
+    print("Syntax: ", sys.argv[0] , " <TF names> <cell_lines> [--no-bg]")
+    quit()
+
+tfs = sys.argv[1]
+cell_lines = sys.argv[2]
+
+if len(sys.argv) == 4 and sys.argv[3] == "--no-bg":
+    add_bg = False
+else:
+    add_bg = True
+
+positives = []
+ambiguous = []
+
+use_hdf5=False
+
+# must run from resultsDir
+
+# loop through the TFs
+#for tf in ['ZNF143']:
+
+def process_tf(tfs, cell_set=None):
+            #           -4   -3    -2          -1
+    #neutrophil-CTCF-human-ENCSR785YRL-optimal_idr.narrowPeak.gz
+    #neutrophil-CTCF-human-ENCSR785YRL-rep1.narrowPeak.gz
+    #neutrophil-CTCF-human-ENCSR785YRL-rep2.narrowPeak.gz
+
+    import glob
+    tf_files = []
+    for tf in tfs:
+        tf_files.extend(glob.glob(dataDir + "*-" + tf + "-human-*-optimal*"))
+
+    count = 0
+    task_list = []
+    for path_name in tf_files:
+        fn = os.path.basename(path_name)
+        fn_list = fn.split('-')
+        exp  = fn_list[-2]
+        tf   = fn_list[-4]
+        cell = '-'.join(fn_list[:-4])
+        if cell_set != None and len(cell_set) != 0: # select cells only in the specified set
+            if not cell in cell_set:
+                continue
+        task_list.append([cell, tf, exp])
+        print(path_name)
+        count = count + 1
+
+    #print(task_list)
+    print(count)
+
+    header = "id"
+    tmp_file_list = []
+    tmp_empty_file = tmpDir + "_tmp_empty_file"
+    tmp_file_list.append(tmp_empty_file)
+    os.system("touch " + tmp_empty_file)
+    has_ambiguous = False
+    for cell, tf, exp in task_list:
+        positive = dataDir + cell + "-" + tf + "-human-" + exp + "-optimal_idr.narrowPeak.gz"
+        if not os.path.isfile(positive):
+            print("ERR does not exist: ", positive)
+        positives.append(positive)
+        header += "\t" + exp
+        merged = tmp_empty_file
+        # merged = ""
+        rep1 = cell + "-" + tf + "-human-" + exp + "-rep1.narrowPeak.gz"
+        rep2 = cell + "-" + tf + "-human-" + exp + "-rep2.narrowPeak.gz"
+
+        has_rep1 = os.path.isfile(dataDir + rep1)
+        has_rep2 = os.path.isfile(dataDir + rep2)
+
+        if has_rep1:
+            merged = dataDir+rep1
+            has_ambiguous = True
+        if has_rep2:
+            merged = dataDir+rep2
+            has_ambiguous = True
+        if has_rep1 and has_rep2:
+            tmp_merged = tmpDir + "_tmp_" + cell + "-" + tf + "-human-" + exp + "-merged.narrowPeak.gz"
+            tmp_file_list.append(tmp_merged)
+            merged = tmp_merged
+            os.system("pigz -d -c " + dataDir+rep1 + " " + dataDir+rep2 + \
+                      " | cut -f 1-3 | bedtools sort | bedtools merge | pigz -c > " + merged)
+        ambiguous.append(merged)
+
+    if positives != []:
+        positives_str = " --positives " + ','.join(positives)
+    else:
+        positives_str = ""
+
+    if has_ambiguous:
+        ambiguous_str = " --ambiguous " + ','.join(ambiguous)
+    else:
+        ambiguous_str = ""
+    
+    background_str = " --background " + genomeDir + "hg19.tsv "
+
+    # call tfdragonn labelregions
+    #
+    # labels_multitask_gz = "tflabel.intervals_file.tsv.gz"
+    # cmd = "tfdragonn labelregions " + positives_str + ambiguous_str + \
+    #       " --genome hg19 --prefix tflabel" #       + " --background background "
+
+    labels_multitask_gz = "label.intervals_file.tsv.gz"
+    cmd = scriptDir + "label_regions " + positives_str + ambiguous_str + \
+          " --genome hg19 --prefix label " + " --stride 20"
+    if add_bg:
+         cmd = cmd + background_str
+    logging.debug(cmd)
+    os.system(cmd)
+
+    labels_multitask    = labels_multitask_gz[:-3]
+
+    os.system("pigz -d -c " + labels_multitask_gz +  " > " + labels_multitask)
+
+    #make the splits
+    valid_chrom = "chr2\t"
+    test_chrom  = "chr1\t"
+
+    os.system("mkdir -p splits")
+    os.system("cat " + labels_multitask + " | grep -P " + valid_chrom + " | pigz -c > splits/valid.tsv.gz")
+    os.system("cat " + labels_multitask + " | grep -P " + test_chrom  + " | pigz -c > splits/test.tsv.gz")
+    cmd = "cat " + labels_multitask + " labels.txt | grep -v -P \"" + test_chrom  + "\|"+ valid_chrom + "\|^id" + "\" | pigz -c > splits/train.tsv.gz"
+    os.system(cmd)
+
+    if use_hdf5 != True:
+        logging.info("prepare_data done")
+        return
+
+    # following preparations are for hdf5
+    tmp_labels_wo_title = tmpDir + "_tmp_labels_without_title.txt"
+    # use sed to change each line from
+    # chr<\t>start<\t>end<\t>label1<\t>label2 ...
+    # to 
+    # chr:start-end<\t>label1<\t>label2 ...
+    os.system("cat " + labels_multitask + " | sed 's/\t/:/; s/\t/-/' > " + tmp_labels_wo_title)
+    tmp_file_list.append(tmp_labels_wo_title)
+
+    os.system("bedtools getfasta -fi " + genomeDir + "hg19.fa -bed " + labels_multitask + " -fo inputs.fa")
+
+    #make the final inputs labels files from the shuffled lines (tfdragonn shuffles already)
+    os.system("echo " + header + " > labels.txt")
+    os.system("cat " + tmp_labels_wo_title + " >> labels.txt")
+
+    #remove the intermediate files
+    for tmp_file in tmp_file_list:
+        os.system("rm -f " + tmp_file)
+
+    logging.info("split and make hdf5")
+    os.system("mkdir -p splits")
+
+    #make the splits
+    os.system("cat labels.txt | grep " + valid_chrom + ": | pigz -c > splits/valid.txt.gz")
+    os.system("cat labels.txt | grep " + test_chrom  + ": | pigz -c > splits/test.txt.gz")
+    cmd = "cat labels.txt | grep -v \"" + test_chrom  + ":\|"+ valid_chrom + ":\|^id" + "\" | pigz -c > splits/train.txt.gz"
+    os.system(cmd)
+
+    os.system("pigz -f labels.txt")
+    #os.system("pigz -f inputs.fa")
+
+    os.system("make_hdf5 --yaml_configs make_hdf5_yaml/* --output_dir .")
+    logging.info("prepare_data done")
+
+
+# guarantee to clean up tmp dir
+import contextlib
+import tempfile
+import shutil
+@contextlib.contextmanager
+def make_temp_directory():
+    temp_dir = tempfile.mkdtemp(dir = ".", prefix = "_tmp_")
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
+
+if __name__ == '__main__':
+
+    with make_temp_directory() as temp_dir:
+        global tmpDir
+        tmpDir = temp_dir + "/"
+
+        tfs = tfs.split(',')
+        if cell_lines == '-' :
+            cell_set = None
+        else:
+            cell_lines = cell_lines.split(',')
+            cell_set = set(cell_lines)
+
+        process_tf(tfs, cell_set)
+
---
+++ scripts/run_pipeline_pf.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# to run the modisco pipeline
+
+import logging
+logging.basicConfig(
+        format='%(asctime)s %(levelname)-5s %(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+import sys
+logging.info(" ".join(sys.argv))
+
+import os
+
+ROOT_DIR    = os.getenv('TFNET_ROOT', "../../") 
+genomeDir   = ROOT_DIR + "/genome/"
+resultDir   = ROOT_DIR + "/results/"
+templateDir = resultDir + "/templates/"
+
+num_task = 5
+end = 100
+
+import sys
+if len(sys.argv) > 6:
+    print("Syntax: ", sys.argv[0] , " <TF names> <cell_lines> <number of tasks> [start]")
+    quit()
+
+tf = sys.argv[1]
+cell_lines = sys.argv[2]
+num_tasks = int(sys.argv[3])
+if num_tasks == 0:
+    print("Syntax: ", sys.argv[0] , " <TF names> <cell_lines> <number of tasks> [start]")
+    quit()
+
+if len(sys.argv) == 5:
+    start = int(sys.argv[4])
+elif len(sys.argv) == 6:
+    start = int(sys.argv[4])
+    end   = int(sys.argv[5])
+else:
+    start = 20 # start with pre-train
+
+
+os.system("mkdir -p logs")
+
+logging.debug("tf=%s, num_tasks=%d start=%d end=%d" % (tf, num_tasks, start, end))
+
+if start <= 10:
+#0 prepare_data with union of positives (no background) and train a model
+    os.system("cp -r " + templateDir + "/config .")
+    os.system("cp -f config/hyperparameter_configs_list_for_pretrain.yaml config/hyperparameter_configs_list.yaml")
+    quit()
+
+if start <= 20 and end > 20:
+    cmd = "python $TFNET_ROOT/scripts/prepare_data_pf.py " + tf + " " + cell_lines + " --no-bg > logs/pre_prepare.log 2>&1"
+    logging.info(cmd)
+    os.system(cmd)
+if start <= 30 and end > 30:
+    os.system("momma_dragonn_train > logs/pre_train.log 2>&1")
+
+    os.system("mkdir -p pretrain")
+    os.chdir("model_files")
+    os.system("cp record_1_*Json.json  record_1_Json.json")
+    os.system("cp record_1_*Weights.h5 record_1_Weights.h5")
+    os.chdir("..")
+    os.system("mv model_files pretrain")
+
+    os.system("gunzip -c splits/test.tsv.gz | sort -k1,1 -k2,2n > subset_nobg.tsv")
+    os.system("bedtools getfasta -fi " + genomeDir + "hg19.fa -bed subset_nobg.tsv -fo subset_nobg.fa")
+    os.system("mv splits pretrain")
+
+    os.system("mv runs_perf-metric-auROC.db pretrain/")
+    os.system("mv label* pretrain/")
+    os.system("mv _tmp_* pretrain/")
+
+    os.system("cp -f config/hyperparameter_configs_list_for_finetune.yaml config/hyperparameter_configs_list.yaml")
+    print("step 0 pre_train done")
+
+#1 prepare_data with background for the main training
+if start <= 40 and end > 40:
+    os.system("python $TFNET_ROOT/scripts/prepare_data_pf.py " + tf + " " + cell_lines + " > logs/prepare.log 2>&1")
+    print("step 1 prepare_data done")
+
+#2 train to continue from pre-trained data
+if start <= 50 and end > 50:
+    os.system("momma_dragonn_train > logs/train.log 2>&1")
+    os.chdir("model_files")
+    os.system("cp record_1_*Json.json  record_1_Json.json")
+    os.system("cp record_1_*Weights.h5 record_1_Weights.h5")
+    os.chdir("..")
+    print("step 2 training done")
+
+#3 deeplift
+if start <= 60 and end > 60:
+    # use the test set as the subset for deeplift
+    # os.system("gunzip -c splits/test.txt.gz | perl -lane 'if ($.%2==1) {print}' | sed 's/:/\t/; s/-/\t/' | sort -k1,1 -k2,2n > splits/subset.tsv") # select half of testset
+    #os.system("gunzip -c splits/test.txt.gz | sed 's/:/\t/; s/-/\t/' | sort -k1,1 -k2,2n > splits/subset.tsv")
+    #os.system("bedtools getfasta -fi " + genomeDir + "hg19.fa -bed splits/subset.tsv -fo subset.fa")
+
+    os.system("python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ subset_nobg.fa " + num_tasks + " > logs/deeplift.log 2>&1")
+    print("step 3 deeplift done")
+
+#4 modisco
+if start <= 70 and end > 70:
+    os.system("python $TFNET_ROOT/scripts/run_tfmodisco.py scores/hyp_scores_task_ subset_nobg.fa subset_nobg.tsv " + num_tasks + " > logs/modisco.log 2>&1")
+
+"""
+'''
+Input for prepare_data.py
+-------
+list of positive  peaks for each task
+list of ambiguous peaks for each task
+fasta of the whole genome
+sizes of the chromosomes
+
+Step 0: prepare_data.py and pretrain
+Step 1: prepare_data.py and train
+-----------------------------------------------------------------------
+'''
+# run prepare_data.py (executable), which calls label_regions script to create labeled bins
+
+'''
+Outputs of prepare_data.py, inputs for momma_dragonn_train
+-------
+inputs.fa           # the input sequences for momma_dragonn_train
+train.hdf5 # training set
+valid.hdf5 # validation set
+test.hdf5  # testing set
+
+
+Step 2: momma_dragonn_train
+-----------------------------------------------------------------------
+'''
+# train momma dragonn model
+
+# re-name momma dragonn model files
+
+'''
+Outputs of momma_dragonn_train (after rename)
+------
+model_files/record_1_Json.json  # model architecture
+model_files/record_1_Weights.h5 # model weights
+
+Additional input for deeplift
+------
+subset.fa      # fasta file for the sequences. either 10% of inputs.fa, or test set (chr1)
+
+'''
+
+'''
+Step 3: run_deeplift.py
+-----------------------------------------------------------------------
+'''
+# run deeplift
+
+'''
+Output from deeplift
+------
+importance scores
+scores/hyp_scores_task_0.npy
+...
+scores/hyp_scores_task_N.npy
+where N= number of tasks
+
+Additional input for modisco:
+------
+subset.txt    # sequences corresponding to subset.fa, excluding the fasta header lines with '>'
+
+Step 4: run_tfmodisco.py
+-----------------------------------------------------------------------
+'''
+# run tf modisco
+
+"""
+
