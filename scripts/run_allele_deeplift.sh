2cbdeafb8b7fdb4161fc0ff572e47cb5b5e760f2
kttian <19katherinet@students.harker.org>
2018-09-25 22:21:00 -0700

    update with multi-task

--- scripts/run_allele_deeplift.sh
+++ scripts/run_allele_deeplift.sh
@@ -2,14 +2,15 @@
 #ln -s SPI1_10k.tsv interpret.tsv
 #bedtools getfasta -fi $TFNET_ROOT/genome/hg19.fa -bed interpret.tsv -fo interpret.fa
 #ln -s /home/ktian/kundajelab/tfnet/results/nandi/SPI1/SPI1_GM12878_refine_18_09_04/model_files .
-python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa0 1 > logs/deeplift0.txt 2>&1
-mv scores/hyp_scores_task_0.npy scores/hyp_scores_task_0.npy0
-python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa1 1 > logs/deeplift1.txt 2>&1
-mv scores/hyp_scores_task_0.npy scores/hyp_scores_task_0.npy1
-python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa2 1 > logs/deeplift2.txt 2>&1
-mv scores/hyp_scores_task_0.npy scores/hyp_scores_task_0.npy2
-python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa3 1 > logs/deeplift3.txt 2>&1
-mv scores/hyp_scores_task_0.npy scores/hyp_scores_task_0.npy3
+task=$1
+python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa0 $task $((task+1)) > logs/deeplift0.txt 2>&1
+mv scores/hyp_scores_task_$task.npy scores/hyp_scores_task_$task.npy0
+python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa1 $task $((task+1)) > logs/deeplift1.txt 2>&1
+mv scores/hyp_scores_task_$task.npy scores/hyp_scores_task_$task.npy1
+python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa2 $task $((task+1)) > logs/deeplift2.txt 2>&1
+mv scores/hyp_scores_task_$task.npy scores/hyp_scores_task_$task.npy2
+python $TFNET_ROOT/scripts/run_deeplift.py model_files/record_1_ scores/interpret.fa3 $task $((task+1)) > logs/deeplift3.txt 2>&1
+mv scores/hyp_scores_task_$task.npy scores/hyp_scores_task_$task.npy3
 
 
 #python get_scores.py 
